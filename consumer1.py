fb4615ab42d9c8c68e09d6b24e16a60df927e60c
AyoDennis <ayodejidennis33@gmail.com>
2025-04-18 16:35:51 +0100

    message acknowledgement loop

--- consumer1.py
+++ consumer1.py
@@ -14,3 +14,10 @@ while True:
     msg = c.poll(1.0)
     time.sleep(2)
 
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+    message = msg.value().decode('utf-8')
+    print(f'Received message from topic => {msg.topic()}, partition => {msg.partition()}')
