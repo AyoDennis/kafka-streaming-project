b8cdc9a3fa7d18b0ed99b577ec7653040bb08e7c
AyoDennis <ayodejidennis33@gmail.com>
2025-04-18 16:44:05 +0100

    consumer2 imports

--- consumer2.py
+++ consumer2.py
@@ -1,26 +1,2 @@
 from confluent_kafka import Consumer
 import time
-
-c = Consumer({
-    'bootstrap.servers': 'localhost:9092',
-    'client.id': 'consumer2',
-    'group.id': 'mygroup',
-    'auto.offset.reset': 'earliest'
-})
-
-c.subscribe(['deji-testing'])
-
-while True:
-    msg = c.poll(1.0)
-    time.sleep(2)
-
-    if msg is None:
-        continue
-    if msg.error():
-        print("Consumer error: {}".format(msg.error()))
-        continue
-    message = msg.value().decode('utf-8')
-    print(f'Received message from topic => {msg.topic()}, partition => {msg.partition()}')
-
-
-c.close()
