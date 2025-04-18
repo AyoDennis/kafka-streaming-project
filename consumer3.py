a8f042820f1229681dec91dc110b8f57cb7522fd
AyoDennis <ayodejidennis33@gmail.com>
2025-04-18 16:49:39 +0100

    consumer3 configuration

--- consumer3.py
+++ consumer3.py
@@ -1,2 +1,9 @@
 from confluent_kafka import Consumer
 import time
+
+c = Consumer({
+    'bootstrap.servers': 'localhost:9092',
+    'client.id': 'consumer3',
+    'group.id': 'mygroup',
+    'auto.offset.reset': 'earliest'
+})
