8a0dca2e4effa3d7a82692ac74802ca771389f33
AyoDennis <ayodejidennis33@gmail.com>
2025-04-18 17:19:36 +0100

    isort and flake 8

--- producer.py
+++ producer.py
@@ -7,27 +7,30 @@ from faker import Faker
 # Configuration
 conf = {'bootstrap.servers': 'localhost:9092',
         'client.id': 'deji_producer',
-        'acks': 'all', # (0, 1, or 'all')
+        'acks': 'all',  # (0, 1, or 'all')
         'compression.type': 'none',
-        'retry.backoff.ms': 1000, # 3 seconds (1ms = 0.001s),
+        'retry.backoff.ms': 1000,  # 3 seconds (1ms = 0.001s),
         # 'retry.backoff.max.ms': 5000,
         # 'message.timeout.ms': 10000,  # Optional: Fail after 10s total
         'retries': 2,
         # 'linger.ms': 100,  # Wait up to 100ms to batch messages
-        # 'batch.num.messages': 1000 # in bytes    
+        # 'batch.num.messages': 1000 # in bytes
         }
 
 # Create Producer instance
 p = Producer(conf)
 
 
-def delivery_report(err, msg): 
+def delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
         Triggered by poll() or flush(). """
     if err is not None:
         print(f'Message delivery failed: {err}')
     else:
-        print(f'At {time.strftime("%H:%M:%S", time.localtime())}, your message was delivered to topic => {msg.topic()}, partition => [{msg.partition()}], offset = {msg.offset()}')
+        print(f'At {time.strftime("%H:%M:%S", time.localtime())}, \
+              your message was delivered to topic => {msg.topic()}, \
+              partition => [{msg.partition()}], offset = {msg.offset()}')
+
 
 sample_data = Faker()
 
@@ -46,4 +49,3 @@ while i <= 100:
     serialize = json.dumps(event)
     p.produce("my_topic", serialize, callback=delivery_report)
     p.flush()
-    
