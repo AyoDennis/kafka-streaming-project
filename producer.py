9544461320952ae0d3b2efc5a9f414c50d40de7a
AyoDennis <ayodejidennis33@gmail.com>
2025-04-18 16:31:29 +0100

    producer update

--- producer.py
+++ producer.py
@@ -43,10 +43,7 @@ while i <= 100:
              }
     i += 1
     time.sleep(2)
-    # print(event)
     serialize = json.dumps(event)
-    p.produce("deji-testing", serialize, callback=delivery_report)
     p.produce("my_topic", serialize, callback=delivery_report)
-#     p.poll()
     p.flush()
-    # p.produce("my-topic", #f"Message {i}")
+    
