ebf737f9849cfbdd36bf66768502ecba863d27b3
AyoDennis <ayodejidennis33@gmail.com>
2025-04-21 14:59:27 +0100

    admin client loop

--- admin_client/admin_client.py
+++ admin_client/admin_client.py
@@ -6,4 +6,9 @@ new_topics = [NewTopic(topic, num_partitions=3, replication_factor=1) for topic 
 
 fs = a.create_topics(new_topics)
 
-
+for topic, f in fs.items():
+    try:
+        f.result() 
+        print("Topic {} created".format(topic))
+    except Exception as e:
+        print("Failed to create topic {}: {}".format(topic, e))
