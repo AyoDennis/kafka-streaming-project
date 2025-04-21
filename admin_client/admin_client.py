1d96f066a38bb499ead1e6c68e59763aba8798f6
AyoDennis <ayodejidennis33@gmail.com>
2025-04-21 14:58:51 +0100

    admin client

--- admin_client/admin_client.py
+++ admin_client/admin_client.py
@@ -2,3 +2,13 @@ from confluent_kafka.admin import AdminClient, NewTopic
 
 a = AdminClient({'bootstrap.servers': 'localhost:9092'})
 
+new_topics = [NewTopic(topic, num_partitions=3, replication_factor=1) for topic in ["my_topic", "my_topic2"]]
+
+fs = a.create_topics(new_topics)
+
+for topic, f in fs.items():
+    try:
+        f.result() 
+        print("Topic {} created".format(topic))
+    except Exception as e:
+        print("Failed to create topic {}: {}".format(topic, e))
