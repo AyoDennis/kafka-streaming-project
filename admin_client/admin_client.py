c2ad824121a533045a85d77224c081f398fdfd10
AyoDennis <ayodejidennis33@gmail.com>
2025-04-21 15:33:12 +0100

    linting

--- admin_client/admin_client.py
+++ admin_client/admin_client.py
@@ -2,13 +2,16 @@ from confluent_kafka.admin import AdminClient, NewTopic
 
 a = AdminClient({'bootstrap.servers': 'localhost:9092'})
 
-new_topics = [NewTopic(topic, num_partitions=3, replication_factor=1) for topic in ["my_topic", "my_topic2"]]
+new_topics = [
+    NewTopic(topic, num_partitions=3, replication_factor=1)
+    for topic in ["my_topic", "my_topic2"]
+    ]
 
 fs = a.create_topics(new_topics)
 
 for topic, f in fs.items():
     try:
-        f.result() 
+        f.result()
         print("Topic {} created".format(topic))
     except Exception as e:
         print("Failed to create topic {}: {}".format(topic, e))
