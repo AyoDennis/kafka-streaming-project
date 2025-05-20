0182ed9881076cdc7caf5ef512041d44f9864263
AyoDennis <ayodejidennis33@gmail.com>
2025-05-20 14:44:29 +0100

    consumer configuration

--- consumers/postgres_consumer.py
+++ consumers/postgres_consumer.py
@@ -10,3 +10,10 @@ from confluent_kafka import Consumer
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
 logging.getLogger().setLevel(20)
+
+consumer_configuration = {
+    'bootstrap.servers': 'localhost:9092',
+    'client.id': 'consumer1',
+    'group.id': 'demo',
+    'auto.offset.reset': 'earliest'
+}
